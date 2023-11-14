dba4e27e5f40bbfc11a643723c27b3d5abde1bd8
alexandru.uioreanu <alexandru.uioreanu@accesa.eu>
2023-11-14 12:58:06 +0200

    Adding some unit test's

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,12 +9,12 @@ plugins {
 
 android {
     namespace = "com.example.fashionday"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.fashionday"
         minSdk = 26
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
@@ -57,9 +57,9 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.10.1")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
-    implementation("androidx.activity:activity-compose:1.7.2")
+    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
+    implementation("androidx.activity:activity-compose:1.8.0")
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
@@ -72,17 +72,19 @@ dependencies {
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.6.4")
+
 
     //hilt
-    implementation("com.google.dagger:hilt-android:2.45")
+    implementation("com.google.dagger:hilt-android:2.48")
     kapt("com.google.dagger:hilt-android-compiler:2.45")
-    kapt("androidx.hilt:hilt-compiler:1.0.0")
-    implementation("androidx.hilt:hilt-navigation-compose:1.1.0-alpha01")
+    kapt("androidx.hilt:hilt-compiler:1.1.0")
+    implementation("androidx.hilt:hilt-navigation-compose:1.1.0")
 
     //retrofit
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
-    implementation("com.squareup.okhttp3:okhttp:4.10.0")
+    implementation("com.squareup.okhttp3:okhttp:4.11.0")
 
     //serialization
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.1")
@@ -102,6 +104,11 @@ dependencies {
     // For control over item selection of both touch and mouse driven selection
     implementation("androidx.recyclerview:recyclerview-selection:1.1.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+
+    testImplementation("io.mockk:mockk-android:1.13.8")
+    testImplementation("io.mockk:mockk-agent:1.13.8")
+    testImplementation("io.mockk:mockk:1.13.8")
+
 }
 
 kapt {
--- app/src/test/java/com/example/fashionday/ExampleUnitTest.kt
+++ app/src/test/java/com/example/fashionday/ExampleUnitTest.kt
@@ -1,17 +1,78 @@
 package com.example.fashionday
 
+import com.example.fashionday.domain.model.Character
+import com.example.fashionday.domain.usecase.FetchAllCharactersUseCase
+import com.example.fashionday.domain.usecase.GetCharacterUseCase
+import com.example.fashionday.ui.screen.main.CharacterUiState
+import com.example.fashionday.ui.screen.main.MainViewModel
+import io.mockk.MockKAnnotations
+import io.mockk.coEvery
+import io.mockk.impl.annotations.RelaxedMockK
+import io.mockk.mockk
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.test.StandardTestDispatcher
+import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.setMain
+import org.junit.After
+import org.junit.Assert.assertEquals
+import org.junit.Before
 import org.junit.Test
-
-import org.junit.Assert.*
 
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
+@OptIn(ExperimentalCoroutinesApi::class)
 class ExampleUnitTest {
-    @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+
+    @RelaxedMockK
+    private val fetchCharactersUseCase = mockk<FetchAllCharactersUseCase>()
+
+    @RelaxedMockK
+    private val getCharacterUseCase = mockk<GetCharacterUseCase>()
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    private val testDispatcher = StandardTestDispatcher()
+
+    @RelaxedMockK
+    private lateinit var viewModel: MainViewModel
+
+    @Before
+    fun setUp() {
+        Dispatchers.setMain(testDispatcher)
+        MockKAnnotations.init(this)
+        viewModel = MainViewModel(fetchCharactersUseCase, getCharacterUseCase)
     }
+
+    @After
+    fun tearDown() {
+        Dispatchers.resetMain() // reset the main dispatcher to the original Main dispatcher
+        testDispatcher.cancel()
+    }
+
+
+    //MainViewModelTest's
+    @Test
+    fun `initial state is correct`() {
+        val initialState = CharacterUiState()
+        assertEquals(initialState.copy(isLoading = true), viewModel.uiState.value)
+    }
+
+    @Test
+    fun `fetchCharacters updates state correctly`() {
+        coEvery { fetchCharactersUseCase() } returns Result.success(listOf<Character>())
+        viewModel.fetchCharacters()
+        assertEquals(
+            CharacterUiState(
+                isLoading = true,
+                characters = emptyList(),
+                filteredCharacters = emptyList()
+            ), viewModel.uiState.value
+        )
+    }
+
+
 }
